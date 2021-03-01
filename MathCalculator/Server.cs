dce0fd251fd4d650c571f00f0eb5eed6abe911cf
Jeppe Hauman <jeppe_hauman@hotmail.com>
2021-03-01 10:50:42 +0100

    Create Server.cs
    Call server created

---
+++ MathCalculator/Server.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MathCalculator
+{
+    public class Server
+    {
+        public void start()
+        {
+            TcpListener server = null;
+
+        }
+
+    }
+}
